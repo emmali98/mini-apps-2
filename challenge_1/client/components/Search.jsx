d050d77392a64a3094051bb04f14e9c01422afb1
Emma Li <emma.m.li.98@gmail.com>
2020-11-04 12:32:11 -0800

    Installs axios, enables request to server for search results

--- challenge_1/client/components/Search.jsx
+++ challenge_1/client/components/Search.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import List from './List.jsx';
 
 const Search = (props) => {
@@ -10,14 +11,22 @@ const Search = (props) => {
   })
 
   const handleClick = (e) => {
-    console.log('testing');
+    axios.get(`/events?q=${query}&_page=1`)
+    .then((res) => {
+      setResults(res.data);
+      // use link header for pagination
+    })
+    .catch((err) => {
+      console.error(err);
+      // TODO: Add something else to indicate to user
+    })
   }
 
   return (
     <div>
       <input type="text" name="search" onChange={(e) => setQuery(e.target.value)} />
       <button onClick={handleClick}>Go</button>
-      <List items={results} />
+      <List events={results} />
     </div>
   )
 }
