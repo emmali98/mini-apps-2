968f260434af93189123f5bda334d10837b125dc
Emma Li <emma.m.li.98@gmail.com>
2020-11-04 11:25:44 -0800

    Sets up barebones html and React component

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+const App = () => {
+  return (
+    <div>TODO</div>
+  )
+}
 
+ReactDOM.render(<App />, document.getElementById('app'));
