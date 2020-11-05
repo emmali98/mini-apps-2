56ca410f07aa4f72e7641d45ebc6618ad1e83d0b
Emma Li <emma.m.li.98@gmail.com>
2020-11-04 17:00:44 -0800

    Implements pagination

--- challenge_1/client/components/Search.jsx
+++ challenge_1/client/components/Search.jsx
@@ -1,20 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import List from './List.jsx';
+import ReactPaginate from 'react-paginate';
 
 const Search = (props) => {
   const [query, setQuery] = useState('');
-  const [results, setResults] = useState([]);
+  const [currentList, setCurrentList] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
 
   useEffect(() => {
     document.title = `${query}`;
   })
 
-  const handleClick = (e) => {
-    axios.get(`/events?q=${query}&_page=1`)
+  const getEvents = (page = 1) => {
+    axios.get(`/events?q=${query}&_page=${page}`)
     .then((res) => {
-      setResults(res.data);
-      // use link header for pagination
+      setCurrentList(res.data);
+
+      let links = res.headers.link.split(', ');
+      let last = links[links.length - 1];
+      setPageCount(last.slice(last.indexOf('page=') + 5, last.indexOf('>;')));
     })
     .catch((err) => {
       console.error(err);
@@ -22,11 +27,22 @@ const Search = (props) => {
     })
   }
 
+  const handlePageClick = (data) => {
+    let selected = data.selected;
+    getEvents(selected + 1);
+  };
+
   return (
     <div>
       <input type="text" name="search" onChange={(e) => setQuery(e.target.value)} />
-      <button onClick={handleClick}>Go</button>
-      <List events={results} />
+      <button onClick={() => getEvents()}>Go</button>
+      <List events={currentList} />
+      <ReactPaginate
+        pageCount={pageCount}
+        pageRangeDisplayed={3}
+        marginPagesDisplayed={2}
+        onPageChange={handlePageClick}
+      />
     </div>
   )
 }
